e04fee8898c3d7e91dbc3383c02928f19e9d0807
jslupicki <jan@slupicki.com>
2019-02-15 20:41:11 +0100

    Update Cucumber version

--- src/test/java/integration/MyStepdefs.java
+++ src/test/java/integration/MyStepdefs.java
@@ -15,9 +15,7 @@ public class MyStepdefs extends SpringIntegrationTest implements En {
     private List<User> userList;
 
     public MyStepdefs() {
-        Before(() -> {
-            userList = null;
-        });
+        Before(() -> userList = null);
         When("^client call get (.*)$", (String path) -> {
             System.out.println("Path:" + path);
             userList = getList(path, new ParameterizedTypeReference<List<User>>() {
@@ -26,9 +24,9 @@ public class MyStepdefs extends SpringIntegrationTest implements En {
         Then("^receiver got list of users$", () -> {
             assertThat(userList).isNotNull();
         });
-        And("^list of users have user with id = (\\d+)$", (Long id) -> {
+        And("^list of users have user with id = (\\d+)$", (Integer id) -> {
             List<Long> idList = userList.stream().map(User::getId).collect(Collectors.toList());
-            assertThat(idList).contains(id);
+            assertThat(idList).contains(id.longValue());
         });
     }
 }
